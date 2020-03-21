9211dc1a6b2f53262a3c472fd140ae5620188fe5
saeveritt <saeveritt@gmail.com>
2020-03-20 20:11:42 -0500

    Temporary update to main

--- main.go
+++ main.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"github.com/saeveritt/go-peerassets/storage"
-	"github.com/saeveritt/go-peerassets/utils"
+	//"github.com/saeveritt/go-peerassets/utils"
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
 )
 
 func init(){
-	utils.ImportRootP2TH()
-	utils.Scan(0)
+	//utils.ImportRootP2TH()
+	//utils.Scan(0)
+	//storage.Connect()
+	//storage.PutRootAsset()
+	//storage.Close()
 }
 
+var(
+)
+
+func getAssets(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	j, _ := storage.GetDecks()
+	w.Write(j)
+
+}
+
+func postAssets(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "post called"}`))
+}
+
+
 func main() {
-	storage.Connect()
-	storage.PutRootAsset()
-	storage.Close()
+	r := mux.NewRouter()
+
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/assets", getAssets).Methods(http.MethodGet)
+	api.HandleFunc("/assets", postAssets).Methods(http.MethodPost)
+	api.HandleFunc("/address", postAssets).Methods(http.MethodPost)
+
+	log.Fatal(http.ListenAndServe(":8089", r))
 }
 
 
