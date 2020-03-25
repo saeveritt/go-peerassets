b293fbbf4acb78806aef5d8bcc3d430fe38a62a1
saeveritt <saeveritt@gmail.com>
2020-03-25 04:57:57 -0500

    update for new api routes

--- api/api.go
+++ api/api.go
@@ -9,44 +9,66 @@ import (
 
 
 func AgaveRouter() *mux.Router {
+	// Create new router
 	r := mux.NewRouter()
+	// Set the subrouter path prefix
 	api := r.PathPrefix("/v1").Subrouter()
+	// Define the function handlers per route
 	api.HandleFunc("/assets", getAssets).Methods(http.MethodGet)
-	api.HandleFunc("/address",getAddress).Methods(http.MethodGet)
+	api.HandleFunc("/transactions",getAddress).Methods(http.MethodGet)
 	return r
 }
 
 func getAddress( w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
+	// Create empty byte array which will store the JSON Response
 	var j []byte
+	// Assign address variable to the value passed in the GET request
 	var address = r.URL.Query().Get("address")
-	j,_ = storage.GetAddress(address)
-	w.Write(j)
+	// If address is not empty
+	if address != ""{
+		// Check for address in storage. Each address has its own dedicated bucket.
+		j, _ = storage.GetAddress(address)
+		// if there was an error writing the JSON byte array,it will send empty array
+		// else it sends a JSON byte array Response with the results
+		w.Write(j)
+	}else{
+		// if address argument is empty, return empty byte array
+		w.Write(j)
+	}
 }
-
-
 
 func getAssets(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var j []byte
 
 	limit := r.URL.Query().Get("limit")
-	l, _ := strconv.Atoi(limit)
+	// Because this is a string, convert l to an integer
+	l, err := strconv.Atoi(limit)
+	// TODO: return error in json
+	if err != nil{w.Write(j)}
 	page := r.URL.Query().Get("page")
-	p, _ := strconv.Atoi(page)
+	// Because this is a string, convert p to an integer
+	p, err := strconv.Atoi(page)
+	// TODO: return error in json
+	if err != nil{w.Write(j)}
 
-	if page == "" && limit == "" {
-		j, _ = storage.GetDecks()
+	if p == 0 && l == 0 {
+		// If page and limit are both 0, return all decks
+		j, _ = storage.GetDecks(0, 0)
 	}
-	if p > 0 && limit == "" {
-		j, _ = storage.GetDecksPages(10, p)
+	if p > 0 && l == 0 {
+		// If page is greater then zero, return page
+		j, _ = storage.GetDecks(10, p)
 	}
-	if l > 0 && page == "" {
-		j, _ = storage.GetDecksPages(l, 1)
+	if p == 0 && l > 0 {
+		// If limit is greater then zero, send one page with l amount
+		j, _ = storage.GetDecks(l, 1)
 	}
 	if p > 0 && l > 0{
-		j, _ = storage.GetDecksPages(l, p)
+		// if both page and l are greater than zero, return l amount on page p
+		j, _ = storage.GetDecks(l, p)
 	}
-
+	// write the JSON to the Response Writer
 	w.Write(j)
 }
--- storage/main.go
+++ storage/main.go
@@ -13,51 +13,97 @@ import (
 var db *bolt.DB
 var err error
 
+func must(err error){
+	// Standard error handler. Will os.exit() on log.Fatal if error occurs.
+	// Only use for db Connection
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
 func Connect() (*bolt.DB,error){
+	// Open the local db file and set it to db. This modifies the global variable so that
+	// functions in this file can use it
 	db, err = bolt.Open("storage/assets.db",0600,&bolt.Options{Timeout: 1 * time.Second})
 	must(err)
 	return db, err
 }
 
 func Close(){
+	// if Connection to db is Open, Close it.
 	if db.GoString() != "" {
 		db.Close()
 	}
 }
 func CreateBucket(bucket string,) {
-
+	// Connect to local db
+	Connect()
+	// Use db.Update and pass a function with a bolt.TX
 	db.Update(func(tx *bolt.Tx) error {
+		// Create the Bucket if it does not exist
 		_, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		// Error Handling, Non-Standard
 		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
+			// Return Bucket Exists
+			return fmt.Errorf("Bucket Exists: %s", err)
 		}
+		// Return nil for Error
 		return nil
 	})
+	// Close local db connection
+	Close()
 }
 
-func GetDecks() ([]byte,error){
+func GetDecks(limit int, page int) ([]byte,error){
 	// Connect to local db
 	Connect()
 	// Create a map, key < Deck ID >, Value < Deck Protobuf >
 	res  := make(map[string]*protobuf.DeckSpawn)
+	bucketName := "DecksProto"
 	// Create a View to query the local database
 	// Input to db.View is a Function that will iterate and grab Keys and Values
-	db.View(func(tx *bolt.Tx) error{
-		// DecksProto bucket contains Key: <Deck ID>, Value: < Deck Protobuf >
-		bucket := tx.Bucket([]byte("DecksProto"))
-		// Setup a return which will iterate through each Key, Value in Bucket and append to map
-		return bucket.ForEach( func(k []byte, v []byte) error{
-			// Grab the <Deck Protobuf> and Parse it into a Deck Object
-			d := protobuf.ParseDeck(v)
-			// Create an entry in the res map where key is string(<Deck ID>)
-			// and the value is set to the Deck Object
-			res[string(k)] = d
-			// Return nil because there were no errors iterating through the bucket
+	if limit == 0 && page == 0 {
+		db.View(func(tx *bolt.Tx) error{
+			// DecksProto bucket contains Key: <Deck ID>, Value: < Deck Protobuf >
+			bucket := tx.Bucket([]byte(bucketName))
+			// Setup a return which will iterate through each Key, Value in Bucket and append to map
+			return bucket.ForEach( func(k []byte, v []byte) error{
+				// Grab the <Deck Protobuf> and Parse it into a Deck Object
+				d := protobuf.ParseDeck(v)
+				// Create an entry in the res map where key is string(<Deck ID>)
+				// and the value is set to the Deck Object
+				res[string(k)] = d
+				// Return nil because there were no errors iterating through the bucket
+				return nil
+			})
+			// Return nil after apending to map
 			return nil
 		})
-		// Return nil after apending to map
-		return nil
-	})
+	} else{
+		// This View is for processing requests with page and limit arguments
+		db.View(func(tx *bolt.Tx) error{
+			// DecksProto bucket contains Key: <Deck ID>, Value: < Deck Protobuf >
+			bucket := tx.Bucket([]byte(bucketName))
+			// Create a counter to restrict which entries from the db we append to Response
+			n := -1
+			return bucket.ForEach( func(k []byte, v []byte) error{
+				// Increase counter
+				n++
+				// Add entries to Response map that are within the terms defined by limit and page
+				if n >= page*limit-limit && n < page * limit {
+					// Grab the <Deck Protobuf> and Parse it into a Deck Object
+					d := protobuf.ParseDeck(v)
+					// Create an entry in the res map where key is string(<Deck ID>)
+					// and the value is set to the Deck Object
+					res[string(k)] = d
+				}
+				// Return nil because there were no errors iterating through the bucket
+				return nil
+			})
+			// Return nil after apending to map
+			return nil
+		})
+	}
 	// Create a variable to store the JSON byte array which will be used to write the Response
 	j,err := json.Marshal(res)
 
@@ -70,53 +116,35 @@ func GetDecks() ([]byte,error){
 	return j, nil
 }
 
-func GetDecksPages(limit int, page int) ([]byte,error){
+
+
+func GetAddress(address string, txtype string)([]byte,error){
+	//make(map[string]string{"card":"Card-","deck":"Deck-"})
+	if txtype == "deck"{
+		res  := make(map[string]*protobuf.DeckSpawn)
+	}
+	else if txtype == "card"{
+		res  := make(map[string]*protobuf.CardTransfer)
+	}
+	else{ return []byte, nil } 
+
 	// Connect to local db
 	Connect()
-	// Create a map, key < Deck ID >, Value < Deck Protobuf >
-	res  := make(map[string]*protobuf.DeckSpawn)
-	// Create a View to query the local database
-	// Input to db.View is a Function that will iterate and grab Keys and Values
-	db.View(func(tx *bolt.Tx) error{
-		bucket := tx.Bucket([]byte("DecksProto"))
-		n := -1 //counter
-		return bucket.ForEach( func(k []byte, v []byte) error{
-			n++
-			if n >= page*limit-limit && n < page * limit {
-				d := protobuf.ParseDeck(v)
-				res[string(k)] = d
-			}
-			return nil
-		})
-
-		return nil
-	})
-	j,err := json.Marshal(res)
-	if err != nil{
-		return j, err
-	}
-	Close()
-	// Will return nil if bucket is not found
-	return j, nil
-}
-
-func GetAddress(address string)([]byte,error){
-	Connect()
-	resD  := make(map[string]*protobuf.DeckSpawn)
-	resC  := make(map[string]*protobuf.CardTransfer)
 
 	db.View(func(tx *bolt.Tx) error{
 		bucket := tx.Bucket([]byte(address))
 		return bucket.ForEach( func(k []byte, v []byte) error{
 
 			if string(k)[0:5] == "Deck-"{
-				resD[string(k[5:])] = protobuf.ParseDeck(v)
+				res[string(k[5:])] = protobuf.ParseDeck(v)
 			}
 			if string(k)[0:5] == "Card-"{
-				resC[string(k[5:])] = protobuf.ParseCard(v)
+				res[string(k[5:])] = protobuf.ParseCard(v)
 			}
 			return nil
 		})
+		// Close local db connection
+		Close()
 		return nil
 	})
 	res := make(map[string]interface{})
@@ -126,7 +154,6 @@ func GetAddress(address string)([]byte,error){
 	if err != nil{
 		return j, err
 	}
-	Close()
 	// Will return nil if bucket is not found
 	return j, nil
 }
@@ -187,11 +214,4 @@ func PrefixScan(bucket string, keyPrefix string) map[string]string{
 		return nil
 	})
 	return M
-}
-
-
-func must(err error){
-	if err != nil{
-		log.Fatal(err)
-	}
 }
