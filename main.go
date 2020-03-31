1b25f997d4464648bf8de4f272b33ac886265e16
saeveritt <saeveritt@gmail.com>
2020-03-31 18:14:33 -0500

    init

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/saeveritt/go-peerassets/api"
+	"github.com/saeveritt/go-peerassets/storage"
+	"github.com/saeveritt/go-peerassets/utils"
 	"log"
 	"net/http"
 )
 
 func init(){
-	//utils.ImportRootP2TH()
-	//storage.PutRootAsset()
+	utils.ImportRootP2TH()
+	storage.PutRootAsset()
 }
 
 func main() {
